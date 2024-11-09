87fe01abfedf4070afff10bbaae6236db13ebcb8
ASHTRICKZ\ashtrickz <ashshvern@gmail.com>
2024-11-09 14:01:47 +0200

    View Bobbing

--- Assets/Scripts/Inventory/ViewBobbing.cs
+++ Assets/Scripts/Inventory/ViewBobbing.cs
@@ -1,49 +1,61 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using Sirenix.OdinInspector;
 using Sirenix.Reflection.Editor;
+using Unity.Collections;
 using Unity.Mathematics;
 using Unity.VisualScripting;
 using UnityEditor.Rendering;
 using UnityEngine;
 
-[RequireComponent(typeof(PositionFollower))]
 public class ViewBobbing : MonoBehaviour
 {
     [SerializeField] private InputManager inputManager;
-    [Space]
-    [SerializeField] private float intensity;
+    [Space] [SerializeField] private float intensity;
     [SerializeField] private float intensityX;
     [SerializeField] private float speed;
 
-    private PositionFollower _follower;
+    [Space, DisplayAsString]
+    [SerializeField] private Vector3 _bobbingOffset;
+
+    private Vector3 _originalPosition;
     private Vector3 _originalOffset;
     private float _sinTime;
 
-    private void Start()
+    private void Awake()
     {
-        _follower = GetComponent<PositionFollower>();
-        _originalOffset = _follower.Offset;
+        _originalPosition = transform.localPosition;
+        _originalOffset = _bobbingOffset;
     }
 
     private void Update()
     {
+        CalculateSinTime();
+        CalculateOffset();
+        UpdatePosition();
+    }
+
+    private void CalculateSinTime()
+    {
         Vector3 inputVector = new Vector3(inputManager.InputAxis.x, 0, inputManager.InputAxis.y);
         if (inputVector.magnitude > 0f)
-        {
             _sinTime += Time.deltaTime * speed;
-        }
+        else if (_sinTime > 1)
+            _sinTime = 1;
         else
-        {
-            _sinTime = 0f;
-        }
-        
-        float sinAmountY = -Mathf.Abs(intensity * Mathf.Sin(_sinTime));
-        Vector3 sinAmountX = _follower.transform.right * intensity * Mathf.Cos(_sinTime) * intensityX;
-
-        _follower.Offset = new Vector3(_originalOffset.x, _originalOffset.y + sinAmountY, _originalOffset.z);
-
-        _follower.Offset += sinAmountX;
-        _follower.Offset = new Vector3(_follower.Offset.x, _follower.Offset.y, _originalOffset.z);
+            _sinTime = Mathf.Lerp(_sinTime, 0, Time.deltaTime * speed);
     }
+
+    private void CalculateOffset()
+    {
+        float sinAmountY = -Mathf.Abs(intensity * Mathf.Sin(_sinTime));
+        Vector3 sinAmountX = transform.right * intensity * Mathf.Cos(_sinTime) * intensityX;
+
+        _bobbingOffset = new Vector3(_originalOffset.x, _originalOffset.y + sinAmountY, _originalOffset.z);
+        _bobbingOffset += sinAmountX;
+    }
+
+    private void UpdatePosition() =>
+        transform.localPosition = _originalPosition + _bobbingOffset;
 }
